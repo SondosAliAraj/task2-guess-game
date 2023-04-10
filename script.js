85805fd1a3cfaf0ada89802fd1cca0196c48c7b9
SondosAliAraj <62075599+SondosAliAraj@users.noreply.github.com>
2023-04-11 01:09:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// Write a program that generates an amount between 500 and 5k and asks the user
+// to guess the check(bill), giving hints if the guess is too high or too low &
+// the user can guess many times & when the user get it right , show him success message.
+
+function guessCheck() {
+  let count = 0; // count how many attempts takes to guess the number
+  const amount = Math.floor(Math.random() * (5000 - 500 + 1) + 500); // return number between 500 - 5k
+
+  //loop untill find the correct guess
+  while (true) {
+    const guess = parseInt(
+      prompt("Guess game! Enter a guess between 500 and 5000:")
+    );
+
+    count++;
+
+    if (isNaN(guess)) {
+      alert("invalid input, please enter a number between 500 and 5000");
+    } else if (guess < 500) {
+      alert("Your guess should be between 500 and 5000. Try again.");
+    } else if (guess < amount) {
+      alert("Your guess is too low. Try again.");
+    } else if (guess > amount) {
+      alert("Your guess is too high. Try again.");
+    } else {
+      alert(
+        `Congratulations! You guessed the amount correctly in ${count} attempts!`
+      );
+      break;
+    }
+  }
+}
+
+guessCheck();
